3bfe2d25c47e1bfffb084010c631becf5dfdfdcc
derselbst <tom.mbrt@googlemail.com>
2017-01-07 16:16:19 +0100

    print strands to stdout

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,27 @@
 using namespace std;
 
 
+void print(const Hair& h)
+{
+    for(size_t s = 0; s<h.size(); s++)
+    {
+        cout << endl;
+        cout << "Strand no. " << s << endl;
+        for(size_t v = 0; v<h[s].size(); v++)
+        {
+            const vec3& pos = h[s][v].Position;
+            
+            cout << "X: " << pos.x << " Y: " << pos.y << " Z: " << pos.z << endl;
+        }
+    }
+}
+
+
 int main()
 {
-    Hair h = HairFactory::GrowHair(10, 10);
+    Hair h = HairFactory::GrowHair(20/*no. strands*/, 10/*no. of vertices per strand*/);
+    
+    print(h);
     
     return 0;
 }
