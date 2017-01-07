b019c243ff210cbd1acaccfa49c115130de0a3eb
derselbst <tom.mbrt@googlemail.com>
2017-01-07 13:33:27 +0100

    use GLM for math calcs

--- src/Vertex.h
+++ src/Vertex.h
@@ -1,20 +1,29 @@
- 
+#pragma once
+
+#include <glm/glm.hpp>
+
+typedef glm::vec3 vec3;
+
 struct Vertex
 {
-    Vertex(vec3 pos, float m);
+    Vertex()
+    {}
+    
+    Vertex(vec3 pos, float m) : Mass{m}, Position{pos}
+    {}
     
     // whether animating this vertex is enabled or not
-    bool IsEnabled;
+    bool IsEnabled{true};
     
     // indicates the mass of this particle
-    float Mass;
+    float Mass{1.0f};
     
-    // holds the current position of the particle within space
-    vec3 Position;
+    // point in 3D space, where this particle is located
+    vec3 Position{0.0f,0.0f,0.0f};
     
     // holds a force that will be applied during a subsequent simulation step of this particle
-    vec3 Force;
+    vec3 Force = vec3(0.0f,0.0f,0.0f);
     
     // we have to remember the velocity applied to this particle during last simulation step
-    vec3 Velocity;
-}
+    vec3 Velocity{0.0f,0.0f,0.0f};
+};
