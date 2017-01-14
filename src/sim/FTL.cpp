b97092dce1b456bba506fe0f5914fc25a667da26
derselbst <tom.mbrt@googlemail.com>
2017-01-14 15:40:31 +0100

    implement FTL more correctly?

--- src/sim/FTL.cpp
+++ src/sim/FTL.cpp
@@ -20,7 +20,8 @@ void FTL::addForce(vec3 f)
 
 void FTL::update()
 {
-  const float TimeStep = 1.0f/10.0f; // mentioned as dt
+  constexpr float TimeStep = 1.0f/20.0f; // mentioned as dt
+  constexpr float Damping = 0.9f; //sDamping
   
     for(size_t s=0; s < this->hair.size(); s++)
     {
@@ -29,25 +30,33 @@ void FTL::update()
 	Vertex& x = this->hair[s][v];
 	
 	// calc new position
-	vec3 p = x.Position + TimeStep * x.Velocity + TimeStep*TimeStep * x.Force;
+	vec3 p = x.Position + TimeStep * x.Velocity + TimeStep*TimeStep * x.Force; // (1)
 	
 	
-	// solve constraints
-	Vertex& pre = this->hair[s][v-1];
-	
-	vec3 pBackup = p; // backup
-	
-	vec3 direction = p - pre.Position;
-	direction = glm::normalize(direction);
-	
-	p = pre.Position + direction * (1.0f/10.0f); // constraint p on a sphere of radius l0 around previous vertex
-	//p.d = pBackup - p.Pos???
+	// solve constraints - (2)
+            Vertex& pre = this->hair[s][v-1];
+            
+            vec3 direction = p - pre.Position;
+            direction = glm::normalize(direction);
+            
+            // backup x's new position before we're going to constraint it
+            vec3 pBackup = p;
+            
+            // constraint the new position of vertex x on a sphere of radius l0 around previous vertex
+            p = pre.Position + direction * pre.L0;
+	// end solve constraint
+        
+        
+        // correction vector for satisfying the constraint
+        vec3 d = pBackup - p; // (6)
 	
 	// update velocity + final position + force
-	x.Velocity = (p - x.Position) / TimeStep;
-	x.Position = p;
+// 	x.Velocity = (p - x.Position) / TimeStep; // (3)
+	x.Velocity = ((p - x.Position) / TimeStep) + Damping * (-d / TimeStep); // (9)
+	
+	x.Position = p; // (4)
+	
 	x.Force = vec3(0,0,0);
       }
     }
-    
 }
