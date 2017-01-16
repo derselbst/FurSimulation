31869efd3ae3cc5623fc468808f38c421f40d20e
derselbst <tom.mbrt@googlemail.com>
2017-01-16 21:04:31 +0100

    update mkvid.sh

--- src/mkvid.sh
+++ src/mkvid.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-tmp=0
+#tmp=0
 
-for f in *png
-do
-	mv $f $(printf %03d $tmp).png
-	tmp=$[tmp + 1]
-done
+#for f in *png
+#do
+#	mv $f $(printf %04d $tmp).png
+#	tmp=$[tmp + 1]
+#done
 
-ffmpeg -framerate 2 %03d.png vid.mp4
+ffmpeg -framerate 30 -start_number 0 -i "%04d.ppm" -threads 4 "dings.mp4"
