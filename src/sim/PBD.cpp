ff044f0875de71c44a869d20e0d6954aeed6c51a
Tom Moebert <tom.moebert@googlemail.com>
2017-01-20 08:34:35 +0100

    update PBD simulation

--- src/sim/PBD.cpp
+++ src/sim/PBD.cpp
@@ -21,6 +21,7 @@ void PBD::addForce(vec3 f)
 void PBD::update()
 {
   constexpr float TimeStep = 1.0f/20.0f; // mentioned as dt
+  constexpr float Damping = 0.96f; //sDamping
   
     for(size_t s=0; s < this->hair.size(); s++)
     {
@@ -39,9 +40,10 @@ void PBD::update()
         {
             Vertex& x = this->hair[s][v];
      
-            vec3 acceleration = x.Force / x.Mass;
+            const vec3 acceleration = x.Force / x.Mass;
             x.Velocity += TimeStep * acceleration;
             x.Position += TimeStep * x.Velocity;
+            x.Force = vec3(0,0,0);
         }
     }
 
@@ -56,8 +58,13 @@ void PBD::update()
                 vec3 direction = x.Position - pre.Position;
                 direction = glm::normalize(direction);
                 
+                // backup x's unconstrained position
+                x.Correction = x.Position;
+
                 // constraint the new position of vertex x on a sphere of radius l0 around previous vertex
                 x.Position = pre.Position + direction * pre.L0;
+
+                x.Correction = x.Position - x.Correction;
             // end solve constraint
         }
     }
@@ -67,7 +74,7 @@ void PBD::update()
       for(size_t v=1 /*skip the very first vertex*/; v < this->hair[v].size(); v++)
         {
             Vertex& x = this->hair[s][v];
-            x.Velocity += (x.Position - x.OldPosition) / TimeStep;
+            x.Velocity += (x.Position - x.OldPosition) / TimeStep + Damping * (x.Correction / TimeStep);
         }
     }
 }
