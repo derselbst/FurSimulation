1f462c31535eb766b407b930a296ae4227a6e584
derselbst <tom.mbrt@googlemail.com>
2017-01-20 23:13:04 +0100

    optimize FTL
    get rid of redundant var

--- src/sim/FTL.cpp
+++ src/sim/FTL.cpp
@@ -40,36 +40,37 @@ Strand* restrict str = this->hair.data();
       #pragma vector aligned
       for(size_t v=nVert-1; v >= 1; v--)
       {
+        x[v].OldPosition = x[v].Position; // (4)
+	
 	// calc new position
-	vec3 p = x[v].Position + TimeStep * x[v].Velocity + TimeStep*TimeStep * x[v].Force; // (1)
+	x[v].Position = x[v].OldPosition + TimeStep * x[v].Velocity + TimeStep*TimeStep * x[v].Force; // (1)
 	
 	
 	// solve constraints - (2)
             Vertex& pre = str[s][v-1];
             
-            vec3 direction = p - pre.Position;
+            vec3 direction = x[v].Position - pre.Position;
             direction = glm::normalize(direction);
             
             // backup x's new position before we're going to constraint it
-            vec3 pBackup = p;
+            vec3 pBackup = x[v].Position;
             
             // constraint the new position of vertex x on a sphere of radius l0 around previous vertex
-            p = pre.Position + direction * pre.L0;
+            x[v].Position = pre.Position + direction * pre.L0;
 	// end solve constraint
             
 	// update velocity + final position + force
-// 	x.Velocity = (p - x.Position) / TimeStep; // (3)
-	x[v].Velocity = ((p - x[v].Position) / TimeStep);
+// 	x.Velocity = (p - x.OldPosition) / TimeStep; // (3)
+	x[v].Velocity = ((x[v].Position - x[v].OldPosition) / TimeStep);
         
         // correct the velocity with the correction vector computed for previous vertex
         // obviously this is not possible for the last particle, so add 0 in this case
         x[v].Velocity += (v+1>=nVert) ? vec3(0,0,0) : Damping * (-x[v+1].Correction / TimeStep); // (9)
 	
-	x[v].Position = p; // (4)
 	
         // correction vector for satisfying the constraint
         // needed when correcting velocity in next iteration
-	x[v].Correction = p - pBackup; // (6)
+	x[v].Correction = x[v].Position - pBackup; // (6)
 	
 	x[v].Force = vec3(0,0,0);
       }
