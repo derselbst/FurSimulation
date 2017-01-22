0c0d49f25f613ba2e1555f42ff9e634a8334516b
derselbst <tom.mbrt@googlemail.com>
2017-01-22 15:39:49 +0100

    got PBD working

--- src/sim/PBD.cpp
+++ src/sim/PBD.cpp
@@ -1,4 +1,5 @@
 
+#define restrict __restrict__
 #include "PBD.h"
 
 PBD::PBD(Hair& h) : ISimulation(h)
@@ -20,12 +21,12 @@ void PBD::addForce(vec3 f)
 
 void PBD::update()
 {
-    constexpr float TimeStep = 1.0f/20.0f; // mentioned as dt
-    constexpr float Damping = 0.96f; //sDamping
+    constexpr float TimeStep = 1.0f/200.0f; // mentioned as dt
+    constexpr float Damping = 0.95f; //sDamping
 
     for(size_t s=0; s < this->hair.size(); s++)
     {
-        for(size_t v=1 /*skip the very first vertex*/; v < this->hair[v].size(); v++)
+        for(size_t v=1 /*skip the very first vertex*/; v < this->hair[s].size(); v++)
         {
             Vertex& x = this->hair[s][v];
 
@@ -36,7 +37,7 @@ void PBD::update()
 
     for(size_t s=0; s < this->hair.size(); s++)
     {
-        for(size_t v=1 /*skip the very first vertex*/; v < this->hair[v].size(); v++)
+        for(size_t v=1 /*skip the very first vertex*/; v < this->hair[s].size(); v++)
         {
             Vertex& x = this->hair[s][v];
 
@@ -49,7 +50,7 @@ void PBD::update()
 
     for(size_t s=0; s < this->hair.size(); s++)
     {
-        for(size_t v=1 /*skip the very first vertex*/; v < this->hair[v].size(); v++)
+        for(size_t v=1 /*skip the very first vertex*/; v < this->hair[s].size(); v++)
         {
             Vertex& x = this->hair[s][v];
             // solve constraints
@@ -64,17 +65,26 @@ void PBD::update()
             // constraint the new position of vertex x on a sphere of radius l0 around previous vertex
             x.Position = pre.Position + direction * pre.L0;
 
+            // correction vector for satisfying the constraint
             x.Correction = x.Position - x.Correction;
             // end solve constraint
         }
     }
 
+    Strand* restrict str = this->hair.data();
     for(size_t s=0; s < this->hair.size(); s++)
     {
-        for(size_t v=1 /*skip the very first vertex*/; v < this->hair[v].size(); v++)
-        {
-            Vertex& x = this->hair[s][v];
-            x.Velocity += (x.Position - x.OldPosition) / TimeStep + Damping * (x.Correction / TimeStep);
+        Vertex* restrict x = str[s].data();
+        const size_t nVert = this->hair[s].size();
+        for(size_t v=1; v < nVert; v++)
+        {            
+            x[v].Velocity = ((x[v].Position - x[v].OldPosition) / TimeStep);
+
+            // correct the velocity with the correction vector computed for previous vertex
+            // obviously this is not possible for the last particle, so add 0 in this case
+            x[v].Velocity += (v+1>=this->hair[v].size()) ? vec3(0,0,0) : Damping * (-x[v+1].Correction / TimeStep); // (9)
+            
+//             x[v].OldPosition = x[v].Position;
         }
     }
 }
